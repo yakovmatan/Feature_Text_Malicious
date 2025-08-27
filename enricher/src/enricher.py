ef9e6966f69f7a32dcb69e1971b3356e479f3964
yakov matan <yakovmatan@gmail.com>
2025-08-27 17:42:22 +0300

    create function to find sesitive

--- enricher/src/enricher.py
+++ enricher/src/enricher.py
@@ -1,5 +1,26 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
 class Enricher:
 
-    def __init__(self, document):
+    def __init__(self):
+        nltk.data.find("sentiment/vader_lexicon.zip")
+        self.document = None
+
+    def receiving_document(self,document):
         self.document = document
+
+    def sentiment_of_text(self):
+        score = SentimentIntensityAnalyzer().polarity_scores(self.document)
+        if score['compound'] >= 0.5:
+            self.document["sentiment"] = 'positive'
+        elif score['compound'] >= -0.49:
+            self.document["sentiment"] =  "neutral"
+        else:
+            self.document["sentiment"] = "negative"
+
+        return self
+
+
 
