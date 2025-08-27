404674fd031ff4f6187c15bb5b4593024f043c3a
tzirkus <zalmen5779@gmail.com>
2025-08-27 18:01:20 +0300

    text processing1

--- processor/src/text_processing.py
+++ processor/src/text_processing.py
@@ -3,6 +3,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
 
 
 
@@ -16,7 +17,9 @@ class TextProcessing:
     def nltk_downloads(self):
         nltk.download('stopwords')
         nltk.download('punkt_tab')
-
+        nltk.download('wordnet')
+        nltk.download('omw-1.4')
+        nltk.download('averaged_perceptron_tagger_eng')
 
     def removes_punctuation_marks(self):
         self.clean_text = re.sub(r'[^\w\s]', '', self.clean_text)
@@ -35,8 +38,37 @@ class TextProcessing:
     def change_to_lower(self):
         self.clean_text = self.clean_text.lower()
 
+    def lemmatization(self):
+        lemmatizer = WordNetLemmatizer()
+        tokens = word_tokenize(self.clean_text)
+        tagged_tokens = pos_tag(tokens)
 
-p = TextProcessing('if, i wont       hey/.')
+        lemmatized_sentence = []
+
+        for word, tag in tagged_tokens:
+            if word.lower() == 'are' or word.lower() in ['is', 'am']:
+                lemmatized_sentence.append(word)
+            else:
+                lemmatized_sentence.append(lemmatizer.lemmatize(word, self.get_wordnet_pos(tag)))
+        self.clean_text = ' '.join(lemmatized_sentence)
+
+    def get_wordnet_pos(self, tag):
+        if tag.startswith('J'):
+            return 'a'
+        elif tag.startswith('V'):
+            return 'v'
+        elif tag.startswith('N'):
+            return 'n'
+        elif tag.startswith('R'):
+            return 'r'
+        else:
+            return 'n'
+
+
+
+
+
+p = TextProcessing('if, i wont Want I wanted    running   hey/.')
 
 p.removes_punctuation_marks()
 print(p.clean_text)
@@ -45,6 +77,10 @@ print(p.clean_text)
 p.removing_unnecessary_whitespace_characters()
 print(p.clean_text)
 p.remove_stopwords()
+print(p.clean_text)
+p.change_to_lower()
+print(p.clean_text)
+p.lemmatization()
 print(p.clean_text)
 
 
