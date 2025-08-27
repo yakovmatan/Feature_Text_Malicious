f3a41c0a8be867dbbf073771fa3f985a31d150a1
yakov matan <yakovmatan@gmail.com>
2025-08-27 15:21:28 +0300

    create docker compose

--- pub_100_mes_at_min/src/manager.py
+++ pub_100_mes_at_min/src/manager.py
@@ -1,7 +1,7 @@
 from fetcher import Fetcher
-from configuration.kafka_configuration import send_event
+from kafka_configuration import send_event
 from data import Data
-import time
+
 
 
 class Manager:
@@ -10,16 +10,10 @@ class Manager:
         self.data = Data(connection=self.connection)
 
 
-    def publish_every_minute(self):
-        while True:
-            documents = self.data.get_100_messages()
-            self.data.split_data_per_category(documents)
-            send_event('raw_tweets_antisemitic', str(self.data.antisemitic))
-            send_event('raw_tweets_not_antisemitic', str(self.data.not_antisemitic))
-            time.sleep(60)
-
-if __name__ == '__main__':
-    manager = Manager()
-    manager.publish_every_minute()
-
-
+    def publish_messages(self):
+        documents = self.data.get_100_messages()
+        for document in documents:
+            if document["Antisemitic"]:
+                send_event('raw_tweets_antisemitic', str(document))
+            else:
+                send_event('raw_tweets_not_antisemitic', str(document))
