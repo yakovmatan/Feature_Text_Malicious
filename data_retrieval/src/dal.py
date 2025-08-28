96c7212299ed16a888bcaf5cd4e7a8fc110e2255
yakov matan <yakovmatan@gmail.com>
2025-08-28 12:19:58 +0300

    create class dal

---
+++ data_retrieval/src/dal.py
@@ -0,0 +1,17 @@
+from pymongo.errors import PyMongoError
+from mongodb_connection import DbConnection
+
+
+
+class Dal:
+
+    def __init__(self, collection1="tweets_antisemitic", collection2='tweets_not_antisemitic'):
+        self.connection = DbConnection()
+        self.collection1 = self.connection.db[collection1]
+        self.collection2 = self.connection.db[collection2]
+
+    def find_all_tweets_antisemitic(self):
+        try:
+            return list(self.collection1.find())
+        except PyMongoError:
+            return {"error": "database_error"}
